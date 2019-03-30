5e5ce34d7f36189be50d0340b1a0d15603712747
rachelwong <hello.rachelwong@gmail.com>
2019-03-30 10:01:46 +1000

    first commit

---
+++ scrabble_control.rb
@@ -0,0 +1,2 @@
+class ScrabbleController
+end
---
+++ scrabble_view.rb
@@ -0,0 +1,17 @@
+class ScrabbleView
+
+    def welcome
+    end
+
+    def goodbye
+    end
+
+    def scoreboard(score)
+    end
+
+    def accept(new_word)
+    end
+
+    def deny(new_word)
+    end
+end
